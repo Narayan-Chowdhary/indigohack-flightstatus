d25ea4cb741944bcc792fedef1e620136c92a81b
Narayan-Chowdhary <narayan97.nk@gmail.com>
2024-07-30 20:11:29 +0530

    updated code of front end after making designning changes

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,7 +6,7 @@ import { useNavigate } from "react-router-dom";
 export default function Header() {
     const nav = useNavigate();
     const [bgColor, setBgColor] = useState('');
-    const [menus, setMenus] = useState()
+    const [menus, setMenus] = useState("text-[#fff]")
 
     useEffect(() => {
         const handleScroll = () => {
