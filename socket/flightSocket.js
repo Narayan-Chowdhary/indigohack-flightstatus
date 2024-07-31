c216a28f5c0c24a3e576ad7168f0e1a7e305de50
Narayan Chowdhary <122730407+Narayan-Chowdhary@users.noreply.github.com>
2024-07-31 09:24:16 +0530

    Update flightSocket.js

--- socket/flightSocket.js
+++ socket/flightSocket.js
@@ -1,28 +1,21 @@
-// Import necessary modules and functions
 const { sendEmail } = require('../utils/email/sendMail');
 const Flight = require("../models/Flight");
 
-// Array to store the previous state of flights for comparison
 let previousFlights = [];
 
-// Function to handle socket connections and flight updates
 const flightSocket = (io) => {
   io.on('connection', (socket) => {
 
-    // Variables for interval and filtering flights
     let interval;
     let filter = 'All Flights';
     let alphanumericRegex = /^[a-zA-Z0-9\s]+$/;
     let alphabeticRegex = /^[a-zA-Z\s]+$/;
 
-    // Function to emit flight updates to the client
     const emitFlightUpdates = async () => {
       try {
-        // Fetch all flights from the database
         const flights = await Flight.find();
         let filteredFlights;
         let newFlight  = null;
-        // Filter flights based on the current filter
         if (filter === 'All Flights') {
           filteredFlights = flights;
         } else if (alphabeticRegex.test(filter)) {
@@ -35,19 +28,15 @@ const flightSocket = (io) => {
           filteredFlights = "No Such Flight Found";
         }
 
-        // Check if the flights data has changed
         if ( JSON.stringify(flights) !== JSON.stringify(previousFlights)) {
-          // Update previous flights and send email notification
           previousFlights = flights;
           const subject = 'Flight Status Update';
           const recipient = process.env.RECIEVER_EMAIL;
           sendEmail(recipient, subject,   filteredFlights);
 
-          // Emit updated flights to the client
           socket.emit('flightStatusUpdate', filteredFlights);
         }
 
-        // Emit updated flights to the client
         socket.emit('flightStatusUpdate', filteredFlights);
       } catch (err) {
         console.error(err);
@@ -55,10 +44,8 @@ const flightSocket = (io) => {
       }
     };
 
-    // Set interval to emit flight updates every 3000ms (3 seconds)
     interval = setInterval(emitFlightUpdates, 3000);
 
-    // Handle client disconnection
     socket.on('disconnect', () => {
       console.log('Client disconnected');
       if (interval) {
@@ -66,7 +53,6 @@ const flightSocket = (io) => {
       }
     });
 
-    // Handle filter updates from the client
     socket.on('setFilter', (newFilter) => {
       filter = newFilter;
       emitFlightUpdates();
@@ -74,5 +60,4 @@ const flightSocket = (io) => {
   });
 };
 
-// Export the flightSocket function
 module.exports = flightSocket;
