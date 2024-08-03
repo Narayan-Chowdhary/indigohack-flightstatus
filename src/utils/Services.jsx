a120d85737202eb78b37726adb95bd6bfc215654
Narayan Chowdhary <122730407+Narayan-Chowdhary@users.noreply.github.com>
2024-08-03 13:29:46 +0530

    Update Services.jsx

--- src/utils/Services.jsx
+++ src/utils/Services.jsx
@@ -1,6 +1,6 @@
 
 
-const BASE_URL = 'http://localhost:5050';
+const BASE_URL = 'https://172.30.151.52:5050';
 const endpoints = {
     getFlightDetails: "/api/flight/status",
     getFlightDetailsById: "/api/flight/id",
